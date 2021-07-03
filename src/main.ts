2467e14a3d6cf52fd94a86ab80b5f824aba2f9e1
mgzu <2931170625@qq.com>
2021-07-03 21:59:30 +0800

    feat: 🎸 add element-pro-components

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import ElementPro from "element-pro-components";
+import "element-pro-components/lib/styles/index.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(ElementPro).mount("#app");
